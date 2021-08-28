048f622e85b500b554298e609c7c72737ed9829e
Umid221 <76803900+Umid221@users.noreply.github.com>
2021-08-28 22:19:48 +0500

    Add files via upload

---
+++ 2ndHomework.py
@@ -0,0 +1,11 @@
+# print(2/3)
+# print("{0:.5} , {1:.3}".format(4/3, 2/3))
+# print("{0:*^11} , {1:╫^9}".format('hello', 'O'))
+# print('backk\bspace')
+# print('tab\ttab')
+# print('new\nline')
+# print('vertical\vtab')
+
+products = input('Xarid qilmoqchi bo\'lgan mahsulotlaringizni kiriting\n>>>')
+print('{0:-^41}'.format('shaxsiy xaridlar ro’yxati'))
+print(products)
