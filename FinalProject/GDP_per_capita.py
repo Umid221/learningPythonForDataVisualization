ad69dfd4022ea0416558cfb61b01e2152b5be6e8
Umid221 <76803900+Umid221@users.noreply.github.com>
2021-11-03 09:16:05 +0500

    Add files via upload

---
+++ FinalProject/GDP_per_capita.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#loading up the csv file
+data = pd.read_csv('D:\programming\python\graduateMonkey\project\countries of the world.csv')
+sorted = data.sort_values('GDP_PC', ascending = False).head(10)
+
+# print(sorted.GDP_PC)
+print(sorted.Country)
+
+labels =[]
+countries = sorted.Country
+for country in countries:
+    country = country[:len(country)-1]
+    if ' ' in country:
+        index = country.index(' ')
+        labels.append(country[0]+ country[index+1])
+        continue
+    labels.append(country[:4])
+value = sorted.GDP_PC
+width = 0.5
+fig, ax = plt.subplots()
+
+ax.bar(labels, value, width)
+ax.set_ylabel('Values')
+ax.set_title('GDP per capita in 2016')
+plt.show()
